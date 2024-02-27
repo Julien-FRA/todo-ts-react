baffc9b2e33195989c2f566ce1e4bfddad2ef63f
Julien-FRA <julien.francois@hetic.net>
2024-02-27 17:07:58 +0100

    <feat>: Add connection + register

--- src/components/form/LoginForm.tsx
+++ src/components/form/LoginForm.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { userSignIn } from "../../utils/api/user.api";
+import { useAuth } from "../../contexts/authContext";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isSignIn, setIsSignIn] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     const form = e.target;
 
-    const elements = form.elements;
-
-    const email = elements.email.value;
-
-    const password = elements.password.value;
-
-    if (email && password) await userSignIn({ email, password });
+    if (!isSignIn) {
+      setIsSignIn(true);
+      await userSignIn({ email, password });
+    }
 
     form.reset();
   };
@@ -23,11 +26,23 @@ const LoginForm = () => {
     <Form className="m-5" onSubmit={handleSubmit}>
       <Form.Group className="mb-3">
         <Form.Label htmlFor="email">Email address</Form.Label>
-        <Form.Control type="email" id="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter email"
+        />
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Label htmlFor="password">Password</Form.Label>
-        <Form.Control type="password" id="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
--- src/components/form/RegisterForm.tsx
+++ src/components/form/RegisterForm.tsx
@@ -1,8 +1,58 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Alert, Button, Container, Form } from "react-bootstrap";
+import { userSignUp } from "../../utils/api/user.api";
+import { redirect } from "react-router-dom";
 
 const RegisterForm = () => {
-  return <Container>RegisterForm</Container>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isSignIn, setIsSignIn] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    if (!isSignIn) {
+      await userSignUp({ email, password })
+        .then(() => setIsSignIn(true))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="email">Email address</Form.Label>
+          <Form.Control
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </Form.Group>
+        {isSignIn && <Alert variant="success">Insciption validé</Alert>}
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default RegisterForm;
